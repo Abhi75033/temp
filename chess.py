f9e64d92c4f299e85374f8bf8d2ee3ef41c6c042
Abhishek Kumar <141398481+Abhi75033@users.noreply.github.com>
2025-02-17 13:41:45 +0530

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,33 +1,33 @@
-def is_safe (board,roe,col,N):
-
-    for I in range(row):
-        if board[I]==col or \
-        board[I]-1== col - row or \
-        board[I]+1== col+ row:
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - (row - i) == col or \
+           board[i] + (row - i) == col:
             return False
-        return True
-    
-    def solve_n_queens(board,row ,N):
-        if row == N:
-            print_solution (board,N)
-            return
-        
-        for col in range(N):
-            if is_safe(board,row,col,N):
-                board[row]=col
-                solve_n_queens(*board,row+1,N)
-                board[row]=-1
-    def print_solution(board,N):
-        print ("\nSolution :")
-        for i in range(N):
-            row = ['.']*N
-            row[board[i]]='Q'
-            print("".join(row))
-            print("\n"+"_"*(2*N))
+    return True
 
-    def n_qeens(N):
-        board =[-1]*N
-        solve_n_queens(board,0,N)
-        
+def solve_n_queens(board, row, N):
+    if row == N:
+        print_solution(board, N)
+        return
     
-        
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_n_queens(board, row + 1, N)
+            board[row] = -1
+
+def print_solution(board, N):
+    print("\nSolution:")
+    for i in range(N):
+        row = ['.'] * N
+        row[board[i]] = 'Q'
+        print(" ".join(row))
+    print("\n" + "-" * (2 * N))
+
+def n_queens(N):
+    board = [-1] * N
+    solve_n_queens(board, 0, N)
+
+# Example usage
+n_queens(4)
